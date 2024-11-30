02d41eb2b1b066fe8a5820c7ea523d9c65379e9a
Wachimingo <joshuaguillen.adoc@live.com>
2024-11-30 15:12:54 -0600

    feat: update new method

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use winit::{
     event::*,
     event_loop::EventLoop,
     keyboard::{KeyCode, PhysicalKey},
-    window::{WindowBuilder, Window},
+    window::{Window, WindowBuilder},
 };
 
 struct State<'a> {
@@ -14,20 +14,77 @@ struct State<'a> {
     window: &'a Window,
 }
 
-imp<'a> State<'a> {
+impl<'a> State<'a> {
     async fn new(window: &'a Window) -> State<'a> {
-        todo!()
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::VULKAN,
+            ..Default::default()
+        });
+
+        let surface = instance.create_surface(window).unwrap();
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    required_features: wgpu::Features::empty(),
+                    required_limits: wgpu::Limits::default(),
+                    label: None,
+                    memory_hints: Default::default(),
+                },
+                None,
+            )
+            .await
+            .unwrap();
+
+        let surface_caps = surface.get_capabilities(&adapter);
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
+        Self {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+        }
     }
+
     pub fn window(&self) -> &Window {
         &self.window
     }
-    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>){
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
         todo!()
     }
     fn input(&mut self, event: &WindowEvent) -> bool {
         todo!()
     }
-    fn update(&mut self) -> {
+    fn update(&mut self) {
         todo!()
     }
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
@@ -35,26 +92,27 @@ imp<'a> State<'a> {
     }
 }
 
-pub fn run() {
+pub async fn run() {
     env_logger::init();
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let mut state = State::new(&window).await;
 
-    event_loop.run(move |event, control_flow| match event {
+    let _ = event_loop.run(move |event, control_flow| match event {
         Event::WindowEvent {
             ref event,
             window_id,
-        } if window_id == window.id() => match event {
+        } if window_id == state.window().id() => match event {
             WindowEvent::CloseRequested
-                | WindowEvent::KeyboardInput {
-                    event:
-                        KeyEvent {
-                            state: ElementState::Pressed,
-                            physical_key: PhysicalKey::Code(KeyCode::Escape),
-                            ..
-                        },
+            | WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        state: ElementState::Pressed,
+                        physical_key: PhysicalKey::Code(KeyCode::Escape),
                         ..
-                } => control_flow.exit(),
+                    },
+                ..
+            } => control_flow.exit(),
             _ => {}
         },
         _ => {}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use wgpu_rust::run;
 
 fn main() {
-    run();
+    pollster::block_on(run());
 }
